d7faad6db5442ce4022948a07d39d247c796b067
shinaps <axon@shinaps.jp>
2024-01-12 23:53:46 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,24 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-const route = app.get('/', (c) => {
+const route = app.get('/overview', (c) => {
   return c.json({
-    message: 'Hello World!',
+    totalRevenue: {
+      value: '$45,231.8',
+      sub: '+20.1% from last month'
+    },
+    subscriptions: {
+      value: '+2350',
+      sub: '+180.1% from last month'
+    },
+    sales: {
+      value: '+12,234',
+      sub: '+19% from last month'
+    },
+    activeNow: {
+      value: '+573',
+      sub: '+201 since last hour'
+    }
   }, 200)
 })
 
