035c44ed43510f5d67147eae7f60192aa0f0ab19
shinaps <axon@shinaps.jp>
2024-01-13 00:07:24 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,54 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-const route = app.get('/overview', (c) => {
-  return c.json({
-    totalRevenue: {
-      value: '$45,231.8',
-      sub: '+20.1% from last month'
-    },
-    subscriptions: {
-      value: '+2350',
-      sub: '+180.1% from last month'
-    },
-    sales: {
-      value: '+12,234',
-      sub: '+19% from last month'
-    },
-    activeNow: {
-      value: '+573',
-      sub: '+201 since last hour'
+const route = app
+  .get('/overview', (c) => {
+    return c.json(
+      {
+        totalRevenue: {
+          value: '$45,231.8',
+          sub: '+20.1% from last month',
+        },
+        subscriptions: {
+          value: '+2350',
+          sub: '+180.1% from last month',
+        },
+        sales: {
+          value: '+12,234',
+          sub: '+19% from last month',
+        },
+        activeNow: {
+          value: '+573',
+          sub: '+201 since last hour',
+        },
+      },
+      200,
+    )
+  })
+  .get('chart-data', (c) => {
+    const getRandomNumber = () => {
+      return Math.floor(Math.random() * 5000) + 1000
     }
-  }, 200)
-})
+    return c.json(
+      {
+        data: [
+          { name: 'Jan', total: getRandomNumber() },
+          { name: 'Feb', total: getRandomNumber() },
+          { name: 'Mar', total: getRandomNumber() },
+          { name: 'Apr', total: getRandomNumber() },
+          { name: 'May', total: getRandomNumber() },
+          { name: 'Jun', total: getRandomNumber() },
+          { name: 'Jul', total: getRandomNumber() },
+          { name: 'Aug', total: getRandomNumber() },
+          { name: 'Sep', total: getRandomNumber() },
+          { name: 'Oct', total: getRandomNumber() },
+          { name: 'Nov', total: getRandomNumber() },
+          { name: 'Dec', total: getRandomNumber() },
+        ],
+      },
+      200,
+    )
+  })
 
 export type AppType = typeof route
 export default app
