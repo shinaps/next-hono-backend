30185e5ab134cb2382ef35520d6ea70ffa419074
shinaps <axon@shinaps.jp>
2024-01-12 23:43:23 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+const route = app.get('/', (c) => {
   return c.json({
     message: 'Hello World!',
   }, 200)
 })
 
-export type AppType = typeof app
+export type AppType = typeof route
 export default app
